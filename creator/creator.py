346bef413847692f968a575fe7a092609a32d9d9
Lekau <lekau9222@gmail.com>
2020-11-20 00:05:30 +0200

    Function outline

---
+++ creator/creator.py
@@ -0,0 +1,18 @@
+password = list()
+def welcome():
+    """
+    docstring
+    """
+    pass
+
+def create_password(parameter_list):
+    """
+    docstring
+    """
+    pass
+
+def success_message():
+    """
+    docstring
+    """
+    pass

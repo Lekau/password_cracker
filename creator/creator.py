b748571c255c07800c0ef79edf16fdd8ca75d935
lmamabol <lmamabol@student.wethinkcode.co.za>
2020-11-20 17:49:54 +0200

    Create package/module needs debugging

--- creator/creator.py
+++ creator/creator.py
@@ -1,18 +1,90 @@
+import string
+from random import choice
 password = list()
+def confirm(message):
+    """
+    Asks the user a question(message) for a y/N and return true if y is 
+    inputted and False if n is inputted
+    Parm: message
+    Return:Bool
+    """
+    ans = ''
+    while ans.isalpha() == False and ans.lower() != 'n' and ans.lower() != 'y':
+        ans = input(message+" Y/n:")
+        if ans.lower() != 'n' and ans.lower() != 'y':
+            return confirm(message)
+        continue
+    if ans.lower() == 'y':
+        return True
+    else:
+        return False
+    
+    
 def welcome():
     """
-    docstring
+    Displays a welcome message stating its creating a password.
+    parm: None
+    Return: None
     """
-    pass
+    print("Welcome to password cracker. We will create a password and crack it.")
 
-def create_password(parameter_list):
+def create_superlist():
     """
-    docstring
+    Creates a list of characters to be used for the password
+    parm: None
+    Return: list
     """
-    pass
+    super_list = list()
+    
+    lower_chars = list(string.ascii_lowercase)
+    upper_chars = list(string.ascii_uppercase)
+    numbers = list(string.digits)
+    special_char = list("#@!()-")
+    
+    has_capital = confirm("Must your password include uppercase letters")
+    has_numbers = confirm("Must your password include numbers")
+    has_special = confirm("Must your password include special characters(# @ ! ( ) -)")
 
+    super_list += lower_chars
+    if has_capital == True:
+        super_list += upper_chars
+    if has_numbers == True:
+        super_list += numbers
+    if has_special == True:
+        super_list += special_char
+    
+    return super_list
+
+def create_password():
+    """
+    Creates and returns a strring password based on the answers 
+    from the input functions ran in it.
+    oarm: None
+    Return: String
+    """
+    print("Lets create a new password")
+    the_password = list()
+    length = 0
+    while length < 4 and length > 20:
+        try:
+            length = int(input("How long should the password be(4 - 20):"))
+            if length >= 4 and length <= 20:
+                break
+            else:
+                continue
+        except Exception:
+            print("Please enter a number between 4 and 20")
+            length = 0
+    big_list = create_superlist() 
+    while length > 0:
+        the_password.append(choice(big_list))
+        length -= 1
+    return the_password
+    
 def success_message():
     """
-    docstring
+    Displays to the stdout that a password has been created.
+    Parm: None
+    Return: None
     """
-    pass
+    print("Password was successfully created.")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cracker.cracker as crack
 import creator.creator as create
-
+import string
 if __name__ == '__main__':
     print("Under Construction")
+    create.welcome()
+    print(create.create_password())

dc6e95a34ab5ee8c4ffc3fe11174d4915e407e40
Lekau <lekau9222@gmail.com>
2020-11-23 23:02:04 +0200

    finished creator package

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import cracker.cracker as crack
 import creator.creator as create
 import string
+import sys
 if __name__ == '__main__':
     print("Under Construction")
     create.welcome()
-    print(create.create_password())
+    my_file = open('hello.txt', "w")
+    my_file.write(''.join(create.create_password()))
+    
+    create.success_message()
